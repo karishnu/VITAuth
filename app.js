3ef449e7a89ef57e1bff2d25382422ebf061de44
Karishnu Poddar <Karishnu Poddar>
2016-08-27 15:21:16 +0530

    invalid creds check

--- app.js
+++ app.js
@@ -4,49 +4,63 @@ var captchaHandler = require("./captcha/captchaHandler");
 var unirest = require('unirest');
 var consts = require('./consts');
 
-function authenticate(regno, pass, callback){
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  var name;
-  var cookieJ = unirest.jar();
+function authenticate(regno, pass, callback) {
 
-  captchaHandler.parseCaptcha(function (captcha, captcha_cookie) {
-    cookieJ.add(unirest.cookie(captcha_cookie), consts.url_login_submit);
+    var name;
+    var cookieJ = unirest.jar();
 
-    unirest.post(consts.url_login_submit)
-        .jar(cookieJ)
-        .form({
-          regno: regno,
-          passwd: pass,
-          vrfcd: captcha
-        })
-        .timeout(28000)
-        .end(onSubmitPost);
-  });
+    captchaHandler.parseCaptcha(function (captcha, captcha_cookie) {
+        cookieJ.add(unirest.cookie(captcha_cookie), consts.url_login_submit);
 
-  const onSubmitPost = function (response) {
+        unirest.post(consts.url_login_submit)
+            .jar(cookieJ)
+            .form({
+                regno: regno,
+                passwd: pass,
+                vrfcd: captcha
+            })
+            .timeout(28000)
+            .end(onSubmitPost);
+    });
 
-    var $ = cheerio.load(response.body);
-    tables = $('table');
-    table = $(tables[1]);
-    children = table.children();
-    child = $(children[0]);
-    user_info_string = child.text().trim();
-    user_info_arr = user_info_string.split(' ');
-    name = user_info_arr[1].trim();
-    for(var k = 2; k<=user_info_arr.length - 5; k++){
-      name = name + " ";
-      name = name + user_info_arr[k];
-    }
-    regno = user_info_arr[user_info_arr.length-3];
-    unirest.get(consts.stud_login_home)
-        .jar(cookieJ)
-        .timeout(28000)
-        .end(onSubmitHome);
-  };
+    const onSubmitPost = function (response) {
 
-  const onSubmitHome = function (response) {
-    callback(name, regno, cookieJ);
-  };
+        //console.log(response.body);
+
+        var $ = cheerio.load(response.body);
+        tables = $('table');
+        table = $(tables[2]);
+
+        tr = $(table).children()['0'];
+        tr_children = $(tr).children();
+
+        if ($($(tr_children['0']).children()['4'])['0'] && $($(tr_children['0']).children()['4'])['0'].attribs.name == "stud_login") {
+            callback(null, null, null, {code: '130', message: 'Invalid Credentials'})
+        }
+        else {
+            table = $(tables[1]);
+            children = table.children();
+            child = $(children[0]);
+            user_info_string = child.text().trim();
+            user_info_arr = user_info_string.split(' ');
+            name = user_info_arr[1].trim();
+            for (var k = 2; k <= user_info_arr.length - 5; k++) {
+                name = name + " ";
+                name = name + user_info_arr[k];
+            }
+            regno = user_info_arr[user_info_arr.length - 3];
+            unirest.get(consts.stud_login_home)
+                .jar(cookieJ)
+                .timeout(28000)
+                .end(onSubmitHome);
+        }
+    };
+
+    const onSubmitHome = function (response) {
+        callback(name, regno, cookieJ, null);
+    };
 }
 
 module.exports = {auth: authenticate};
--- consts.js
+++ consts.js
@@ -8,3 +8,5 @@ exports.captcha_parse_error_code = 400;
 exports.captcha_parse_error_message = 'Captcha could not be parsed.';
 exports.home_blocked_code = 500;
 exports.home_blocked_message = 'Your Student Login seems to have been blocked.';
+exports.invalid_creds_code = 600;
+exports.invalid_creds_message = 'Wrong Registration Number or Password';
