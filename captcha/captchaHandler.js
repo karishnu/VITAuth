c0a5c79ff7ec30946a61a8b67123a7c0f5b5453a
Karishnu Poddar <karishnu@gmail.com>
2017-01-28 23:23:14 +0530

    captcha fix

--- captcha/captchaHandler.js
+++ captcha/captchaHandler.js
@@ -5,13 +5,23 @@ var consts = require('../consts');
 function parseCaptcha(callback) {
 
     const onCaptchaRequest = function (response) {
-        var pixelmap = captchaParser.getPixelMapFromBuffer(response.body);
 
-        const key = Object.keys(response.cookies)[0];
-        const cookieSerial = key + "=" + response.cookies[key];
-        console.log(cookieSerial);
+        if (response.body == null) {
+            unirest.get(consts.captcha_link)
+                .encoding(null)
+                .timeout(26000)
+                .end(onCaptchaRequest);
+        }
+        else {
 
-        return callback(captchaParser.getCaptcha(pixelmap), cookieSerial);
+            var pixelmap = captchaParser.getPixelMapFromBuffer(response.body);
+
+            const key = Object.keys(response.cookies)[0];
+            const cookieSerial = key + "=" + response.cookies[key];
+            console.log(cookieSerial);
+
+            return callback(captchaParser.getCaptcha(pixelmap), cookieSerial);
+        }
     };
 
     unirest.get(consts.captcha_link)
